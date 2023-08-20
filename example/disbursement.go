19bc3482f96a62b0e7f0d82e92b2e2892e6985e8
Abdul Hamid <abdul.surel@gmail.com>
2023-08-20 11:58:26 +0700

    example: Add fetch disbursement items & fetch disbursement

--- example/disbursement.go
+++ example/disbursement.go
@@ -106,3 +106,39 @@ func ExampleApproveDisbursement() {
 	// Will be return response from DurianPay
 	fmt.Println(res)
 }
+
+func ExampleFetchDisbursementItemsByID() {
+	c := client.NewClient(client.Options{
+		ServerKey: "xxx-xxx",
+	})
+
+	// With params
+	opt := durianpay.FetchDisbursementItemsOption{
+		Skip:  10,
+		Limit: 10,
+	}
+	res, err := c.Disbursement.FetchDisbursementItemsByID(context.TODO(), "dis_xxx", &opt)
+	if err != nil {
+		// Handle error
+	}
+
+	// Without params
+	// res, err := c.Disbursement.FetchDisbursementItemsByID(context.TODO(), "dis_xxx", nil)
+
+	// Will be return response from DurianPay
+	fmt.Println(res)
+}
+
+func ExampleFetchDisbursementByID() {
+	c := client.NewClient(client.Options{
+		ServerKey: "xxx-xxx",
+	})
+
+	res, err := c.Disbursement.FetchDisbursementByID(context.TODO(), "dis_xxx")
+	if err != nil {
+		// Handle error
+	}
+
+	// Will be return response from DurianPay
+	fmt.Println(res)
+}
