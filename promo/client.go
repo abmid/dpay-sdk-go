52f8b69c97c3120b56b1175a08ec262cba57b59a
Abdul Hamid <abdul.surel@gmail.com>
2023-09-03 22:54:58 +0700

    Feat: Add fetch promos
    Now Promos Fetch API supports on client promo

--- promo/client.go
+++ promo/client.go
@@ -23,7 +23,7 @@ const (
 	PATH_PROMO = durianpay.DURIANPAY_URL + "/v1/merchants/promos"
 )
 
-// Create return a response from Create Promos API
+// Create return a response from Create Promos API.
 //
 //	[Doc Create Promos API]: https://durianpay.id/docs/api/promos/create/
 func (c *Client) Create(ctx context.Context, payload durianpay.PromoPayload) (*Promo, *durianpay.Error) {
@@ -37,4 +37,20 @@ func (c *Client) Create(ctx context.Context, payload durianpay.PromoPayload) (*P
 	}
 
 	return &res.Data, nil
+}
+
+// FetchPromos return a response from Promos Fetch API.
+//
+//	[Doc Promos Fetch API]: https://durianpay.id/docs/api/promos/fetch/
+func (c *Client) FetchPromos(ctx context.Context) ([]Promo, *durianpay.Error) {
+	res := struct {
+		Data []Promo `json:"data"`
+	}{}
+
+	err := c.Api.Req(ctx, http.MethodGet, PATH_PROMO, nil, nil, nil, &res)
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Data, nil
 }
--- promo/client_test.go
+++ promo/client_test.go
@@ -145,3 +145,96 @@ func TestClient_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestClient_FetchPromos(t *testing.T) {
+	featureWrap := tests.FeatureWrap(t)
+	defer featureWrap.Ctrl.Finish()
+
+	type args struct {
+		ctx context.Context
+	}
+	tests := []struct {
+		name    string
+		args    args
+		prepare func(m mocks, args args)
+		wantRes []Promo
+		wantErr *durianpay.Error
+	}{
+		{
+			name: "Success",
+			args: args{
+				ctx: context.Background(),
+			},
+			prepare: func(m mocks, args args) {
+				m.api.EXPECT().
+					Req(gomock.Any(), "GET", PATH_PROMO, nil, nil, nil, gomock.Any()).
+					DoAndReturn(func(ctx context.Context, method string, url string, param any, body any, header map[string]string, response any) *durianpay.Error {
+						err := json.Unmarshal(featureWrap.ResJSONByte(path_response_promo+"fetch_promos_200.json"), response)
+						if err != nil {
+							panic(err)
+						}
+
+						return nil
+					})
+			},
+			wantRes: []Promo{
+				{
+					Currency:     "IDR",
+					Label:        "SALE502022",
+					StartsAt:     tests.StringToTime("2022-02-24T18:30:00Z"),
+					EndsAt:       tests.StringToTime("2022-02-27T18:30:00Z"),
+					Discount:     "10",
+					DiscountType: "percentage",
+					Type:         "card_promos",
+					PromoDetails: PromoDetails{
+						PromoID:   "prm_3eTlttAEF84045",
+						BinList:   []int{424242},
+						BankCodes: []string{},
+					},
+					SubType:            "direct_discount",
+					LimitType:          "quota",
+					LimitValue:         "100",
+					PriceDeductionType: "total_price",
+					Status:             "expired",
+					CreatedAt:          tests.StringToTime("2023-09-03T03:27:06.882206Z"),
+					UpdatedAt:          tests.StringToTime("2023-09-03T03:27:06.882206Z"),
+					IsLive:             false,
+					ID:                 "prm_3eTlttAEF84045",
+				},
+			},
+		},
+		{
+			name: "Internal Server Error",
+			args: args{
+				ctx: context.Background(),
+			},
+			prepare: func(m mocks, args args) {
+				m.api.EXPECT().
+					Req(gomock.Any(), "GET", PATH_PROMO, nil, nil, nil, gomock.Any()).
+					Return(durianpay.FromAPI(500, featureWrap.ResJSONByte(path_response+"internal_server_error_500.json")))
+			},
+			wantErr: durianpay.FromAPI(500, featureWrap.ResJSONByte(path_response+"internal_server_error_500.json")),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiMock := mock_common.NewMockApi(featureWrap.Ctrl)
+			parseArgs := tt.args
+
+			c := &Client{
+				ServerKey: featureWrap.ServerKey,
+				Api:       apiMock,
+			}
+
+			tt.prepare(mocks{api: apiMock}, parseArgs)
+
+			gotRes, gotErr := c.FetchPromos(parseArgs.ctx)
+			if !reflect.DeepEqual(gotRes, tt.wantRes) {
+				t.Errorf("Client.FetchPromos() gotRes = %v, wantRes %v", gotRes, tt.wantRes)
+			}
+			if !reflect.DeepEqual(gotErr, tt.wantErr) {
+				t.Errorf("Client.FetchPromos() gotErr = %v, wantErr %v", gotErr, tt.wantErr)
+			}
+		})
+	}
+}
